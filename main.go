71cc4b6c559413c8106f23a833cd89e5af8b3be4
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-02-01 15:03:57 +0300

    chore: made modifications for railway

--- main.go
+++ main.go
@@ -3,20 +3,39 @@ package main
 import (
 	"geek-stash/repository"
 	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Unable to load environmental variables")
-	}
+	loadEnv()
 	repo := repository.InitRepo()
 
 	app := fiber.New()
 
 	repo.SetupRoutes(app)
 
-	app.Listen(":8080")
+	app.Listen("0.0.0.0:"+getPort())
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		return port
+	}
+	return port
+}
+
+func loadEnv() {
+	env := os.Getenv("GO_ENV")
+	if env == "" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatalf("Unable to load environmental variables %v", err)
+		}
+	}
+	// if production i.e railway, do nothing
 }
