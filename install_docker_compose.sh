4afb7d5bbcb7f1d6e2ca764ea6f2dba433640177
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-02-16 23:05:21 +0300

    fix: check version

--- install_docker_compose.sh
+++ install_docker_compose.sh
@@ -7,6 +7,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   sudo curl -L "https://github.com/docker/compose/releases/download/3.8/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
   echo "Docker Compose installation complete."
+  docker-compose --version  # Check Docker Compose version
+  which docker-compose  # Check Docker Compose path
 else
   echo "Docker Compose is already installed."
+    docker-compose --version  # Check Docker Compose version
+    which docker-compose  # Check Docker Compose path
 fi
