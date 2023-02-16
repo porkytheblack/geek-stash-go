f225574629ff5b8ff6ca826b33c292a9ff10f110
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-02-16 20:53:38 +0300

    feature: added workflow

--- api-frontend/src/layout/home.tsx
+++ api-frontend/src/layout/home.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 type tProps = {
@@ -9,6 +10,12 @@ function Home( props: tProps ) {
     const { children } = props
   return (
     <div className="grid items-center justify-center">
+        <Image
+          src={`/photo.jpeg`}
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
         {
             children
         }
