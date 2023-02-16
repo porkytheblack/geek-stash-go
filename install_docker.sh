256f038772e376c22e99416125bd8aea09590390
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-02-16 23:23:23 +0300

    fix: new install docker file

---
+++ install_docker.sh
@@ -0,0 +1,30 @@
+
+# check if docker is installed
+
+if ! [ -x "$(command -v docker)" ]; then
+    # install docker
+    echo "Installing docker"
+    sudo apt-get update
+    sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+
+    sudo mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    sudo docker run hello-world
+
+else
+echo "Docker is already installed"
+fi
+
